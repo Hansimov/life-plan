b939387427385fa4c31e998cc37750a9d8a91dfe
asimov <591172499@qq.com>
2022-05-15 16:13:10 +0800

    x Improve calendar plot feature

--- data_vis.py
+++ data_vis.py
@@ -3,22 +3,55 @@ import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
 import pandas as pd
 
-df = pd.read_csv("data.csv")
-cols_rename_dict = {
-    "日期" : "date"     ,
-    "编程" : "code"     ,
-    "交易" : "trade"    ,
-    "健身" : "bodybuild",
-    "读书" : "read"     ,
-}
-df.rename(columns=cols_rename_dict, inplace=True)
-df["date"] = pd.to_datetime(df["date"])
 
-fig, ax = plt.subplots()
-ax.set_aspect(1)
+class DataParser:
+    def __init__(self, data_filename="data.csv"):
+        self.df = pd.read_csv(data_filename)
+        cols_rename_dict = {
+            "日期": "date",
+            "编程": "code",
+            "交易": "trade",
+            "健身": "body",
+            "读书": "read",
+        }
+        self.df.rename(columns=cols_rename_dict, inplace=True)
+        self.df["date"] = pd.to_datetime(self.df["date"])
 
-for idx, row in df.iterrows():
-    weekday = row["date"].isoweekday()
-    ax.add_patch(Rectangle((2 * idx, weekday), 1, 1, color="blue"))
-ax.plot()
-# plt.show()
+
+class CalendarPlotter:
+    def __init__(self, data_filename="data.csv"):
+        self.aspect = 8
+        self.action_color_dict = {
+            "code": "#0000FF",
+            "trade": "#00FF00",
+            "body": "#FF00FF",
+            "read": "#FFFF00",
+        }
+        self.df = DataParser(data_filename).df
+
+    def plot(self):
+        fig, ax = plt.subplots()
+        ax.set_aspect(self.aspect)
+        row0 = self.df.iloc[[0]]
+        wk_offset = row0["date"][0].isoweekday() - 1
+        for ida, row in self.df.iterrows():
+            for idb, (key, val) in enumerate(self.action_color_dict.items()):
+                rect_x_len = self.aspect
+                rect_y_len = 1
+                rect_x = rect_x_len * ((idb % 2) + 2.5 * ((ida + wk_offset) // 7))
+                rect_y = rect_y_len * ((idb // 2) - 2.5 * ((ida + wk_offset) % 7))
+                facecolor = val if row[key] == 1 else "white"
+                rect = Rectangle(
+                    (rect_x, rect_y),
+                    rect_x_len,
+                    rect_y_len,
+                    facecolor=facecolor,
+                    edgecolor="gray",
+                )
+                ax.add_patch(rect)
+        ax.plot()
+        # plt.show()
+
+
+calendar_plotter = CalendarPlotter()
+calendar_plotter.plot()
