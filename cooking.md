bf725d211f7aed10ab0f239c6c5832b58c3532cb
asimov <591172499@qq.com>
2022-05-21 15:57:52 +0800

    + Add month/day texts to calendar

---
+++ cooking.md
@@ -0,0 +1,17 @@
+## Links
+* 教你怎么做饭之电饭煲使用教程
+  * https://www.bilibili.com/video/BV1Rf4y1h7e2
+* 大米粥的正确做法，出锅后黏稠又顺滑，非常的好喝
+  * https://www.bilibili.com/video/BV1K5411A7U1
+* 煮大米粥，牢记3个小窍门，大米下锅就开花飘香，黏稠顺滑很实用
+  * https://www.bilibili.com/video/BV1Pp4y1z77d
+
+
+## 煮粥
+* 米:水 = 1:8
+* 米：1/2 杯
+
+
+## 煮饭
+* 米:水 = 1:1.2
+* 米（1杯=1碗）：1杯
--- data_vis.py
+++ data_vis.py
@@ -2,9 +2,10 @@
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle, Patch
 import pandas as pd
+import datetime
 
 plt.rcParams["font.sans-serif"] = ["SimHei"]
-plt.rc('axes', unicode_minus=False)
+plt.rc("axes", unicode_minus=False)
 
 
 class DataParser:
@@ -35,19 +36,20 @@ class CalendarPlotter:
         self.df = self.data_parser.df
 
     def plot(self):
-        fig, ax = plt.subplots()
+        fig, ax = plt.subplots(dpi=200)
         ax.set_aspect(self.aspect)
 
         # Plot daily blocks
         row0 = self.df.iloc[[0]]
         wk_offset = row0["date"][0].isoweekday() - 1
+        rect_x_len = 1
+        rect_y_len = rect_x_len / self.aspect
         for ida, row in self.df.iterrows():
-            for idb, (key, val) in enumerate(self.action_color_dict.items()):
-                rect_x_len = 1
-                rect_y_len = rect_x_len / self.aspect
+            for idb, (action, color) in enumerate(self.action_color_dict.items()):
+                # Plot color blocks
                 rect_x = rect_x_len * ((idb % 2) + 2.5 * ((ida + wk_offset) // 7))
                 rect_y = rect_y_len * ((idb // 2) - 2.5 * ((ida + wk_offset) % 7))
-                facecolor = val if row[key] == 1 else "white"
+                facecolor = color if row[action] == 1 else "white"
                 rect = Rectangle(
                     (rect_x, rect_y),
                     *(rect_x_len, rect_y_len),
@@ -56,12 +58,28 @@ class CalendarPlotter:
                 )
                 ax.add_patch(rect)
 
+            # Plot day and month texts
+            date = row["date"]
+            day_text_x = rect_x + rect_x_len / 10
+            day_text_y = rect_y + rect_y_len / 8
+            date_text_str = f"{date.day:02}"
+            plt.text(day_text_x, day_text_y, date_text_str)
+
+            if date.isoweekday() == 7:
+                month_text_x = rect_x
+                month_text_y = rect_y - rect_y_len * 2
+                month_text_str = f"{date.month}"
+                # month_text_str = date.strftime("%b")
+                plt.text(month_text_x, month_text_y, month_text_str, ha="center")
+
         # Plot legends
         handles = []
-        for idb, (key, val) in enumerate(self.action_color_dict.items()):
-            patch = Patch(color=val, label=self.data_parser.action_en_zh_dict[key])
+        for idb, (action, color) in enumerate(self.action_color_dict.items()):
+            patch = Patch(color=color, label=self.data_parser.action_en_zh_dict[action])
             handles.append(patch)
         plt.legend(handles=handles, bbox_to_anchor=(1.05, 1))
+
+        ax.axis("off")
         ax.plot()
         # plt.show()
         plt.savefig("calendar.png")
